224454059da065541f3ae6269a8981419a7d818f
Valery Alikin <weblcw@gmail.com>
2018-09-09 19:19:19 +0500

    added starting map animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,46 @@
-
 $(document).ready(function(){
     function moveUp(thisObject){
-        thisObject.appendTo(thisObject.parents('svg>g'));
+        thisObject.appendTo(thisObject.parents('.map-full>g'));
     }
 
-    $('svg g').hover(function() {
+    $('.map-full g').hover(function() {
         moveUp($(this));
+    });
+
+    $('.start .btn').click(function()  {
+        anime({
+            targets: '.overlay',
+            opacity: 0,
+            duration: 600,
+            easing: 'easeInOutCubic',
+        });
+
+        anime({
+            targets: '.map-full',
+            width: 100,
+            duration: 600,
+            easing: 'easeInOutCubic',
+        });
+
+        anime({
+            targets: '.start',
+            opacity: 0,
+            duration: 600,            
+            easing: 'easeInOutCubic',
+            complete: function () {
+                $('.start').remove();
+            }
+        });
+
+        $('.map-full').addClass('show');
+
+        // var tl = anime.timeline();
+        // tl.add({
+        //     targets: '.map-full',
+        //     opacity: 1,
+        //     width: '72%',
+        //     duration: 600,
+        //     easing: 'easeInOutCubic',            
+        // })
     });
 });
